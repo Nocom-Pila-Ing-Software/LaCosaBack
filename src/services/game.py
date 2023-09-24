ac4a74d687f89d5d8594e74d87c05837e4ebbd20
Lautaro Bachmann <laurigbach@gmail.com>
2023-09-24 17:00:15 -0300

    Add docstrings and type hints

--- src/services/game.py
+++ src/services/game.py
@@ -5,8 +5,13 @@ from models import WaitingRoom, Game, Player
 from schemas.game import GameCreationRequest
 
 
-def create_deck(game):
-    # create game deck
+def create_deck(game: Game) -> None:
+    """
+    Create a deck of cards for the given game
+
+    Args:
+    game (Game): The game object to add the deck to
+    """
     for i in range(40):
         if i % 2 == 0:
             game.cards.create(name="Lanzallamas")
@@ -14,8 +19,16 @@ def create_deck(game):
             game.cards.create(name="Carta Mazo")
 
 
-def assign_thing_role(game):
-    # Assign roles
+def assign_thing_role(game: Game) -> Player:
+    """
+    Assign "thing" role to a random player within a game
+
+    Args:
+    game (Game): The game object to assign roles in
+
+    Returns:
+    Player: The player that was assigned the "thing" role
+    """
     players_in_game = list(select(p for p in game.players))
 
     the_thing = random.choice(players_in_game)
@@ -23,14 +36,25 @@ def assign_thing_role(game):
     return the_thing
 
 
-def deal_cards_to_thing(the_thing):
-    # Deal cards
+def deal_cards_to_thing(the_thing: Player) -> None:
+    """
+    Deal cards to the "thing" player
+
+    Args:
+    the_thing (Player): The player to deal cards to
+    """
     the_thing.cards.create(name="La Cosa")
     for _ in range(3):
         the_thing.cards.create(name="Carta Mano")
 
 
-def deal_cards_to_players(game):
+def deal_cards_to_players(game: Game) -> None:
+    """
+    Deal cards to all "human" players within a game
+
+    Args:
+    game (Game): The game object to deal cards in
+    """
     human_players = list(
         select(p for p in game.players if p.role == "human")
     )
@@ -40,7 +64,16 @@ def deal_cards_to_players(game):
             player.cards.create(name="Carta Mano")
 
 
-def create_game_on_db(creation_request: GameCreationRequest):
+def create_game_on_db(creation_request: GameCreationRequest) -> Game:
+    """
+    Creates a new Game instance in the database
+
+    Args:
+    creation_request (GameCreationRequest): Input data to create the game
+
+    Returns:
+    Game: The newly created game object
+    """
     room = WaitingRoom[creation_request.roomID]
     game = Game(
         waiting_room=room,
@@ -49,11 +82,29 @@ def create_game_on_db(creation_request: GameCreationRequest):
     return game
 
 
-def is_room_valid(creation_request):
+def is_room_valid(creation_request: GameCreationRequest) -> bool:
+    """
+    Checks if the room id in creation_request exists in database
+
+    Args:
+    creation_request (GameCreationRequest): Input data to validate
+
+    Returns:
+    bool: True if room exists, False otherwise
+    """
     return WaitingRoom.get(id=creation_request.roomID) is not None
 
 
-def are_players_valid(creation_request):
+def are_players_valid(creation_request: GameCreationRequest) -> bool:
+    """
+    Checks if every player in the creation_request exists in database
+
+    Args:
+    creation_request (GameCreationRequest): Input data to validate
+
+    Returns:
+    bool: True if all players exist, False otherwise
+    """
     for player in creation_request.players:
         if Player.get(id=player.playerID) is None:
             return False
@@ -61,7 +112,17 @@ def are_players_valid(creation_request):
     return True
 
 
-def handle_errors(creation_request: GameCreationRequest):
+def handle_errors(creation_request: GameCreationRequest) -> None:
+    """
+    Checks for errors in creation_request and raises HTTPException if needed
+
+    Args:
+    creation_request (GameCreationRequest): Input data to validate
+
+    Raises:
+    HTTPException(status_code=404): If the room ID doesn't exist in database
+    HTTPException(status_code=400): If a player ID doesn't exist in database
+    """
     if not is_room_valid(creation_request):
         raise HTTPException(
             status_code=404, detail="Room ID doesn't exist"
