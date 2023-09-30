b486ed77fc72675be140ed1b422cb5fc85f091ba
ValentinoMensio <valentino.mensio@mi.unc.edu.ar>
2023-09-29 22:17:31 -0300

    Cambios sugeridos realizados

--- src/routers/room.py
+++ src/routers/room.py
@@ -2,35 +2,22 @@ from fastapi import APIRouter, status
 from schemas.player import PlayerID, PlayerName
 from pony.orm import db_session
 from models import WaitingRoom
-from services.room_operations import (create_player, 
-                                    player_name_valid, 
-                                    player_exists_in_database, 
-                                    waiting_room_exists, 
-                                    player_added_to_room)
+import services.room_operations as room_ops
 
 room_router = APIRouter()
 
 @room_router.post("/{roomID}/players", response_model=PlayerID, status_code=status.HTTP_200_OK)
 async def add_player_to_waiting_room(request_data: PlayerName, roomID: int) -> PlayerID:
     with db_session:
-        # Obtener el playerName del cuerpo de la solicitud
         player_name = request_data.playerName
-        # Verificar que el playerName no esté vacío
-        player_name_valid(player_name)
-        # Verificar que la sala exista
-        waiting_room_exists(roomID)
-        # Obtener el roomID de la ruta
+        room_ops.check_valid_player_name(player_name)
+        room_ops.check_waiting_room_exists(roomID)
         room = WaitingRoom.get(id=roomID)
-        # Crear un nuevo jugador
-        player = create_player(player_name, room)
-        # Verificar que el jugador se creó correctamente
-        player_exists_in_database(player)
-        # Agregar jugador a la sala
+        player = room_ops.create_player(player_name, room)
+        room_ops.check_player_exists_in_database(player)
         room.players.add(player)
-        # Verificar que el jugador se agregó correctamente a la sala
-        player_added_to_room(player, room)
+        room_ops.is_player_added_to_room(player, room)
 
-        # Retornar el ID del jugador
         response = PlayerID(playerID=player.id)
 
         return response
--- src/tests/test_room.py
+++ src/tests/test_room.py
@@ -2,7 +2,6 @@ from main import app
 from fastapi.testclient import TestClient
 from models import Player, WaitingRoom, db
 from pony.orm import db_session
-from .util import setup_test_db
 import pytest
 
 client=TestClient(app)
@@ -11,29 +10,20 @@ client=TestClient(app)
 @pytest.fixture()
 def setup_database():
     global db
-    if db is None:
-        db = setup_test_db()
-    else:
-        # Limpiar todas las tablas antes de cada módulo
-        db.drop_all_tables(with_all_data=True)
-        db.create_tables()
-        with db_session:
-            WaitingRoom(id=0, room_name="Test_room")
-            WaitingRoom(id=1, room_name="Test_room2")
+    db.drop_all_tables(with_all_data=True)
+    db.create_tables()
+    with db_session:
+        WaitingRoom(id=0, room_name="Test_room")
+        WaitingRoom(id=1, room_name="Test_room2")
     yield
 
 # Test para la función add_player_to_waiting_room con una room valida
 def test_add_player_to_waiting_room_valid(setup_database):
-    # Realiza una solicitud POST a la ruta /room/0/players
     response = client.post("/room/0/players", json={"playerName": "Test_player"})
 
-    # Verifica que la respuesta tenga un código de estado 200 (OK)
     assert response.status_code == 200
-
-    # Verifica que la respuesta contenga el ID del jugador
     assert response.json() == {"playerID": 1}
 
-    # Verifica que el jugador se haya agregado correctamente a la sala de espera
     with db_session:
         waiting_room = WaitingRoom.get(id=0)
         player = Player.get(id=1)
@@ -41,13 +31,10 @@ def test_add_player_to_waiting_room_valid(setup_database):
 
 # Test para la función add_player_to_waiting_room con una room invalida
 def test_add_player_to_waiting_room_invalid(setup_database):
-    # Realiza una solicitud POST a la ruta /room/1/players
     response = client.post("/room/999/players", json={"playerName": "Test_player"})
 
-    # Verifica que la respuesta tenga un código de estado 404 (Not Found)
     assert response.status_code == 404
 
-    # verifica que el usuario no se haya creado
     with db_session:
         player = Player.get(id=1)
         assert player is None
@@ -55,20 +42,15 @@ def test_add_player_to_waiting_room_invalid(setup_database):
 
 # Test para la funcion añadir muchos jugadores a una sala
 def test_add_many_players_to_waiting_room(setup_database):
-    # Realiza uchas solicitudes POST a la ruta /room/0/players
     response = client.post("/room/0/players", json={"playerName": "Test_player1"})
     response = client.post("/room/0/players", json={"playerName": "Test_player2"})
     response = client.post("/room/0/players", json={"playerName": "Test_player3"})
     response = client.post("/room/0/players", json={"playerName": "Test_player4"})
     response = client.post("/room/0/players", json={"playerName": "Test_player5"})
 
-    # Verifica que la respuesta tenga un código de estado 200 (OK)
     assert response.status_code == 200
-
-    # Verifica que la respuesta contenga el ID del jugador
     assert response.json() == {"playerID": 5}
 
-    # Verifica que todo los jugadroes se hayan agregado correctamente a la sala de espera
     for i in range(1, 6):
         with db_session:
             waiting_room = WaitingRoom.get(id=0)
@@ -77,26 +59,20 @@ def test_add_many_players_to_waiting_room(setup_database):
 
 # Test para la funcion llamar add player sin ningun parametro
 def test_add_player_to_waiting_room_bad_request(setup_database):
-    # Realiza una solicitud POST a la ruta /room/0/players
     response = client.post("/room/0/players", json={})
 
-    # Verifica que la respuesta tenga un código de estado 422 (Unprocessable Entity)
     assert response.status_code == 422
 
-    # verifica que el usuario no se haya creado
     with db_session:
         player = Player.get(id=1)
         assert player is None
 
 # Test para la funcion añadir jugador sin nombre
 def test_add_player_to_waiting_room_no_name(setup_database):
-    # Realiza una solicitud POST a la ruta /room/0/players
     response = client.post("/room/0/players", json={"playerName": ""})
 
-    # Verifica que la respuesta tenga un código de estado 400 (Bad Request)
     assert response.status_code == 400
 
-    # verifica que el usuario no se haya creado
     with db_session:
         player = Player.get(id=1)
         assert player is None
