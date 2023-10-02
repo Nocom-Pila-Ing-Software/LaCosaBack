49cd64dfe6a02949e0646b5dcfab8c6419f6e16f
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-02 16:47:52 -0300

    Change api name and description Fixes NP-40

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
 """Project constants file."""
-PROJECT_NAME = "Agenda API"
-PROJECT_DESCRIPTION = "A simple Agenda API"
+PROJECT_NAME = "La Cosa API"
+PROJECT_DESCRIPTION = "API para el juego de cartas online 'La Cosa'"
 DB_PROVIDER = "sqlite"
