fa2ac6d5737edb147f14d409662a861007b1cd84
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-13 10:52:20 -0300

    Make adding new cards easier

--- src/lacosa/game/utils/card_effects.py
+++ src/lacosa/game/utils/card_effects.py
@@ -1,4 +1,5 @@
 from models import Player, Game
+from collections.abc import Callable
 
 
 def update_player_positions_after_death(player, game):
@@ -11,3 +12,15 @@ def update_player_positions_after_death(player, game):
 def apply_lanzallamas_effect(target_player: Player, game: Game) -> None:
     target_player.is_alive = False
     update_player_positions_after_death(target_player, game)
+
+
+def do_nothing(target_player: Player, game: Game):
+    pass
+
+
+def get_card_effect_function(card_name: str) -> Callable[[Player, Game], None]:
+    match card_name:
+        case "Lanzallamas":
+            return apply_lanzallamas_effect
+        case _:
+            return do_nothing
