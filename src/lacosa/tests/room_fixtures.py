6d59a2f525ca90a469034d09ff14ffa835d2a964
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-20 12:54:57 -0300

    Create players with positions on db in room fixture

--- src/lacosa/tests/room_fixtures.py
+++ src/lacosa/tests/room_fixtures.py
@@ -1,6 +1,7 @@
-from models import WaitingRoom, Player, db
+from models import WaitingRoom, Game, db
 from pony.orm import db_session
 import pytest
+
 
 @pytest.fixture()
 def db_room_creation():
@@ -9,14 +10,19 @@ def db_room_creation():
 
     with db_session:
         room = WaitingRoom(id=0, room_name="Test Room2")
-        room.players.create(id=1, username="Test_player1", is_host=True, position=1)
+        room.players.create(id=1, username="Test_player1",
+                            is_host=True, position=1)
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture()
 def db_room_creation_with_players():
     db.drop_all_tables(with_all_data=True)
     db.create_tables()
     with db_session:
         room = WaitingRoom(id=0, room_name="Test_room")
-        room.players.create(id=1, username="Test_player", is_host=True)
-        room.players.create(id=2, username="Test_player2", is_host=False)
-        room.players.create(id=3, username="Test_player3", is_host=False)
+        room.players.create(id=1, username="Test_player",
+                            is_host=True, position=1)
+        room.players.create(id=2, username="Test_player2",
+                            is_host=False, position=2)
+        room.players.create(id=3, username="Test_player3",
+                            is_host=False, position=3)
