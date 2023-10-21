7bbe79d2fae4093e97d863fc4a96206d46852ab9
ValentinoMensio <valentino.mensio@mi.unc.edu.ar>
2023-10-20 21:03:02 -0300

    Revert "implementacion parcial de Cambio de lugar"
    This reverts commit 2c12c3a8c66c89f4571cb510b06d785dcc502d94.

--- src/lacosa/game/utils/card_effects.py
+++ src/lacosa/game/utils/card_effects.py
@@ -11,17 +11,10 @@ def update_player_positions_after_death(player, game):
             p.position -= 1
     player.position = 0
 
-def swap_player_positions(player1, player2):
-    player1.position, player2.position = player2.position, player1.position
 
 def apply_lanzallamas_effect(target_player: Player, game: Game) -> None:
     target_player.is_alive = False
     update_player_positions_after_death(target_player, game)
-
-def apply_cambio_de_lugar_effect(target_player: Player, game: Game) -> None:
-    swap_player_positions = swap_player_positions(target_player, game.current_player)
-    # evento para intercambiar carta con el jugador adyasente desde esta posicion
-    # preguntar gonza
 
 
 def do_nothing(target_player: Player, game: Game):
@@ -30,8 +23,7 @@ def do_nothing(target_player: Player, game: Game):
 
 def get_card_effect_function(card_name: str) -> CardEffectFunc:
     _card_effects: Dict[str, CardEffectFunc] = {
-        "Lanzallamas": apply_lanzallamas_effect,
-        "Cambio de lugar": apply_cambio_de_lugar_effect
+        "Lanzallamas": apply_lanzallamas_effect
     }
 
     return _card_effects.get(card_name, do_nothing)
