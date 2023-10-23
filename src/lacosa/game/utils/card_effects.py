b09f9b7321a1baf04d77d2ddec06db0aa8defad5
ValentinoMensio <valentino.mensio@mi.unc.edu.ar>
2023-10-23 13:21:57 -0300

    Nada de barbacoa effect

--- src/lacosa/game/utils/card_effects.py
+++ src/lacosa/game/utils/card_effects.py
@@ -41,7 +41,8 @@ def get_card_effect_function(card_name: str) -> CardEffectFunc:
         "Lanzallamas": apply_lanzallamas_effect,
         "Cambio de lugar": apply_switch_position_cards_effect,
         "Más vale que corras": apply_switch_position_cards_effect,
-        "Aquí estoy bien": do_nothing
+        "Aquí estoy bien": do_nothing,
+        "Nada de barbacoas": do_nothing
     }
 
     return _card_effects.get(card_name, do_nothing)
