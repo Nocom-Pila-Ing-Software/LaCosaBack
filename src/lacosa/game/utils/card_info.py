9197ddf9b8080ca9b71223d7db224f91558c1dd4
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2023-10-23 08:22:14 -0300

    Development of the get defense cards endpoint

--- src/lacosa/game/utils/card_info.py
+++ src/lacosa/game/utils/card_info.py
@@ -4,7 +4,7 @@ from lacosa import utils
 from lacosa.game.utils.deck import Deck
 import lacosa.game.utils.exceptions as exceptions
 from lacosa.interfaces import ResponseInterface
-from lacosa.player.schemas import UsabilityActionResponse, UsabilityActionInfoCard
+from lacosa.player.schemas import UsabilityActionResponse, UsabilityActionInfoCard, UsabilityResponse, UsabilityInfoCard
 from fastapi import status
 
 class CardUsabilityInformer(ResponseInterface):
@@ -17,7 +17,7 @@ class CardUsabilityInformer(ResponseInterface):
         Returns the information of which cards can be played or discarded by the player
 
         Returns:
-        UsabilityResponse: The cards information
+        UsabilityInfoResponse: The cards information
         """
 
         return UsabilityActionResponse(cards=self.get_cards_info())
@@ -72,3 +72,57 @@ class CardUsabilityInformer(ResponseInterface):
 
         exceptions.validate_player_in_game(None, self.player, status.HTTP_400_BAD_REQUEST)
         exceptions.validate_player_alive(self.player)
+
+class CardDefenseInformer(ResponseInterface):
+    def __init__(self, player_id: int, card_id: int):
+        self.player = utils.find_player(player_id)
+        self.card = utils.find_card(card_id)
+        self.handle_errors()
+
+    def get_response(self) -> UsabilityResponse:
+        """
+        Returns the information of which cards can be used to defend against the card played by the attacker
+
+        Returns:
+        UsabilityResponse: The cards information
+        """
+
+        return UsabilityResponse(cards=self.get_cards_info())
+    
+    def get_cards_info(self) -> list:
+        """
+        Returns the information of which cards can be used to defend against the card played by the attacker
+
+        Returns:
+        list: The cards information
+        """
+
+        cards_info = []
+        for card in self.player.cards:
+            if card.name in self.get_cards_that_defend(self.card.name):
+                cards_info.append(UsabilityInfoCard(
+                    cardID=card.id,
+                    name=card.name,
+                    description=card.description,
+                    usable=True
+                ))
+    
+    def get_cards_that_defend(self, card_name: str) -> str:
+        """
+        Returns the type of the card
+        """
+        config_path = Path(__file__).resolve().parent.parent / 'utils' / 'config_deck.json'
+
+        with open(config_path) as config_file:
+            config = json.load(config_file)
+
+        return config["cards"][card_name]["defensible_by"]
+
+    def handle_errors(self) -> None:
+        """
+        Checks for errors and raises HTTPException if needed
+        """
+
+        exceptions.validate_player_in_game(None, self.player, status.HTTP_400_BAD_REQUEST)
+        exceptions.validate_player_alive(self.player)
+        exceptions.validate_card_type(self.card, "action", status.HTTP_400_BAD_REQUEST)
