3e636f133f71e87b8ae1b785ad256031f3a4b548
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-28 09:58:03 -0300

    Move CardPlayer and CardDefender to other modules

---
+++ src/lacosa/game/utils/card_trader.py
@@ -0,0 +1,92 @@
+from models import Event
+from ..schemas import PlayCardRequest, EventTypes, GenericCardRequest, EventCreationRequest
+from lacosa.game.utils.deck import Deck
+from lacosa.game.utils.event_creator import EventCreator
+from .card_effects import execute_card_effect
+import lacosa.utils as utils
+import lacosa.game.utils.exceptions as exceptions
+from .deck import Deck
+from lacosa.interfaces import ActionInterface
+import lacosa.game.utils.turn_handler as turn_handler
+from pathlib import Path
+import json
+from pony.orm import commit, select
+
+
+class CardTrader(ActionInterface):
+    def __init__(self, trade_request: GenericCardRequest, game_id: int):
+        self.game = utils.find_game(game_id)
+        self.event = utils.find_partial_event(trade_request.playerID)
+        self.player = utils.find_player(trade_request.playerID)
+        self.card = utils.find_card(trade_request.cardID)
+        self.handle_errors()
+
+    def execute_action(self) -> None:
+        """
+        If both players have selected a card, trade them
+        If only one player has selected a card, save it in the event
+
+        Args:
+        trade_request (PlayCardRequest): Input data to validate
+        game_id (int): The id of the game to validate
+        """
+
+        if self.event.card1 is None:
+            self.event.card1 = self.card
+            self.game.current_player = self.event.player2.id
+        else:
+            events_completed = select(
+                e for e in self.game.events if e.is_completed == True)[:]
+            last_event = None
+            if len(events_completed) > 0:
+                last_event = sorted(events_completed, key=lambda e: e.id)[-1]
+            self.event.card2 = self.card
+            self.event.is_completed = True
+            self.event.is_successful = True
+            if last_event is not None and last_event.type == "action" and last_event.is_successful == True and (last_event.card1.name == "Cambio de lugar" or "Mas vale que corras") and last_event.player1.id == self.event.player1.id:
+                self.game.current_player = last_event.player2.id
+            else:
+                self.game.current_player = self.get_next_player_id()
+            self.game.current_action = "draw"
+
+            self.event.player1.cards.remove(self.event.card1)
+            self.event.player1.cards.add(self.event.card2)
+
+            self.event.player2.cards.remove(self.event.card2)
+            self.event.player2.cards.add(self.event.card1)
+
+            if(self.event.player1.role == "thing" and self.event.card1.name == "Infeccion"):
+                self.event.player2.role = "infected"
+            elif(self.event.player2.role == "thing" and self.event.card2.name == "Infeccion"):
+                self.event.player1.role = "infected"
+
+            commit()
+
+
+    def get_next_player_id(self):
+        next_player = turn_handler.get_next_player(
+            self.game,
+            self.event.player1.position
+        )
+        return next_player.id
+
+    def handle_errors(self) -> None:
+        """
+        Checks for errors in trade_request and raises HTTPException if needed
+
+        Args:
+        trade_request (PlayCardRequest): Input data to validate
+        game_id (int): The id of the game to validate
+
+        Raises:
+        HTTPException(status_code=400): If the player is not found or the card is not in the player's hand
+        HTTPException(status_code=403): If the player is not allowed to trade
+        HTTPException(status_code=404): If the game is not found
+        """
+        exceptions.validate_player_in_game(self.game, self.player)
+        exceptions.validate_current_action(self.game, "trade")
+        exceptions.validate_current_player(self.game, self.player)
+        exceptions.validate_player_alive(self.player)
+        exceptions.validate_player_has_card(self.player, self.card.id)
+        exceptions.validate_card_allowed_to_trade(self.card, self.event, self.player)
+
--- src/lacosa/game/utils/game_actions.py
+++ src/lacosa/game/utils/game_actions.py
@@ -12,169 +12,6 @@ from pathlib import Path
 import json
 from pony.orm import commit, select
 
-class CardPlayer(ActionInterface):
-    def __init__(self, play_request: PlayCardRequest, game_id: int):
-        self.game = utils.find_game(game_id)
-        self.player = utils.find_player(play_request.playerID)
-        self.target_player = utils.find_player(play_request.targetPlayerID)
-        self.card = utils.find_card(play_request.cardID)
-        self.handle_errors()
-
-    def execute_action(self) -> None:
-        # Creo un evento de tipo acción
-        self.game.last_played_card = self.card
-
-        # Creo el evento de acción
-        event_request = EventCreationRequest(
-            gameID=self.game.id,
-            playerID=self.player.id,
-            targetPlayerID=self.target_player.id,
-            cardID=self.card.id,
-            targetCardID=-1,
-            type=EventTypes.action,
-            isCompleted=False,
-            isSuccessful=False
-        )
-        event_create = EventCreator(event_request)
-        event_create.create()
-        event = utils.find_partial_event(self.player.id)
-
-        Deck.discard_card(self.card, self.player, self.game)
-
-        check_card_is_defensible = self.check_card_is_defensible(self.card)
-        if not check_card_is_defensible:
-            execute_card_effect(self.card, self.player,
-                                self.target_player, self.game)
-
-            event.is_completed = True
-            event.is_successful = True
-
-            self.game.current_action = "trade"
-            if select(e for e in Event if (e.player1.id == self.player.id or e.player2.id == self.target_player.id) and e.is_completed == False).get() is None:
-                event_request = EventCreationRequest(
-                    gameID=self.game.id,
-                    playerID=self.player.id,
-                    targetPlayerID=self.get_next_player_id(),
-                    cardID=-1,  # probar
-                    targetCardID=-1,
-                    type=EventTypes.trade,
-                    isCompleted=False,
-                    isSuccessful=False
-                )
-
-                event_create = EventCreator(event_request)
-                event_create.create()
-        else:
-            self.game.current_action = "defense"
-            self.game.current_player = self.target_player.id
-
-    def get_next_player_id(self):
-        next_player = turn_handler.get_next_player(
-            self.game,
-            self.player.position
-        )
-        return next_player.id
-
-    def check_card_is_defensible(self, card) -> bool:
-        config_path = Path(__file__).resolve().parent.parent / \
-            'utils' / 'config_deck.json'
-
-        with open(config_path) as config_file:
-            config = json.load(config_file)
-
-        if card.name in config['cards']:
-            if 'defensible_by' in config['cards'][card.name]:
-                return True
-        return False
-
-    def handle_errors(self) -> None:
-        """
-        Checks for errors in play_request and raises HTTPException if needed
-        """
-
-        exceptions.validate_player_in_game(self.game, self.player)
-        exceptions.validate_player_in_game(self.game, self.target_player)
-        exceptions.validate_player_has_card(self.player, self.card.id)
-        exceptions.validate_player_alive(self.target_player)
-
-
-class CardTrader(ActionInterface):
-    def __init__(self, trade_request: GenericCardRequest, game_id: int):
-        self.game = utils.find_game(game_id)
-        self.event = utils.find_partial_event(trade_request.playerID)
-        self.player = utils.find_player(trade_request.playerID)
-        self.card = utils.find_card(trade_request.cardID)
-        self.handle_errors()
-
-    def execute_action(self) -> None:
-        """
-        If both players have selected a card, trade them
-        If only one player has selected a card, save it in the event
-
-        Args:
-        trade_request (PlayCardRequest): Input data to validate
-        game_id (int): The id of the game to validate
-        """
-
-        if self.event.card1 is None:
-            self.event.card1 = self.card
-            self.game.current_player = self.event.player2.id
-        else:
-            events_completed = select(
-                e for e in self.game.events if e.is_completed == True)[:]
-            last_event = None
-            if len(events_completed) > 0:
-                last_event = sorted(events_completed, key=lambda e: e.id)[-1]
-            self.event.card2 = self.card
-            self.event.is_completed = True
-            self.event.is_successful = True
-            if last_event is not None and last_event.type == "action" and last_event.is_successful == True and (last_event.card1.name == "Cambio de lugar" or "Mas vale que corras") and last_event.player1.id == self.event.player1.id:
-                self.game.current_player = last_event.player2.id
-            else:
-                self.game.current_player = self.get_next_player_id()
-            self.game.current_action = "draw"
-
-            self.event.player1.cards.remove(self.event.card1)
-            self.event.player1.cards.add(self.event.card2)
-
-            self.event.player2.cards.remove(self.event.card2)
-            self.event.player2.cards.add(self.event.card1)
-
-            if(self.event.player1.role == "thing" and self.event.card1.name == "Infeccion"):
-                self.event.player2.role = "infected"
-            elif(self.event.player2.role == "thing" and self.event.card2.name == "Infeccion"):
-                self.event.player1.role = "infected"
-
-            commit()
-
-
-    def get_next_player_id(self):
-        next_player = turn_handler.get_next_player(
-            self.game,
-            self.event.player1.position
-        )
-        return next_player.id
-
-    def handle_errors(self) -> None:
-        """
-        Checks for errors in trade_request and raises HTTPException if needed
-
-        Args:
-        trade_request (PlayCardRequest): Input data to validate
-        game_id (int): The id of the game to validate
-
-        Raises:
-        HTTPException(status_code=400): If the player is not found or the card is not in the player's hand
-        HTTPException(status_code=403): If the player is not allowed to trade
-        HTTPException(status_code=404): If the game is not found
-        """
-        exceptions.validate_player_in_game(self.game, self.player)
-        exceptions.validate_current_action(self.game, "trade")
-        exceptions.validate_current_player(self.game, self.player)
-        exceptions.validate_player_alive(self.player)
-        exceptions.validate_player_has_card(self.player, self.card.id)
-        exceptions.validate_card_allowed_to_trade(self.card, self.event, self.player)
-
 
 class CardDefender(ActionInterface):
     def __init__(self, defend_request: GenericCardRequest, game_id: int):
@@ -207,7 +44,6 @@ class CardDefender(ActionInterface):
 
                 Deck.discard_card(self.card, self.event.player2, self.game)
 
-
         elif self.event.type == "action":
             if self.card is not None:
                 self.event.card2 = self.card
@@ -221,9 +57,9 @@ class CardDefender(ActionInterface):
                 self.event.is_successful = True
 
             self.event.is_completed = True
-            
+
             self.game.last_played_card = self.card
-            
+
             self.game.current_action = "trade"
 
             self.game.current_player = self.event.player1.id
