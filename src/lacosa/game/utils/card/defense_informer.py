1e66fa93793ec8c68f1afc85c1f2f67c31484542
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-29 18:09:29 -0300

    Refactor card defense informer

--- src/lacosa/game/utils/card/defense_informer.py
+++ src/lacosa/game/utils/card/defense_informer.py
@@ -19,65 +19,59 @@ class CardDefenseInformer(ResponseInterface):
         self.handle_errors()
 
     def get_response(self) -> UsabilityResponse:
-        """
-        Returns the information of which cards can be used to defend against the card played by the attacker
+        card_info = self.get_defense_cards_info()
+        return UsabilityResponse(cards=card_info)
 
-        Returns:
-        UsabilityResponse: The cards information
-        """
-
-        return UsabilityResponse(cards=self.get_cards_info())
-
-    def get_cards_info(self) -> list:
-        """
-        Returns the information of which cards can be used to defend against the card played by the attacker
-
-        Returns:
-        list: The cards information
-        """
-
-        cards_info = []
-        # Get the cards that only are usable if the player is the target of the trade event (and only in a trade event)
+    def get_defense_cards_info(self) -> list:
         if self.card is None:
-            event = select(e for e in Event if e.type == "trade" and (
-                e.player1 == self.player or e.player2 == self.player) and e.is_completed is False).first()
-            # Verify if the player is the target of the trade event and not the trader generating the event
-            if event is not None and event.player2 == self.player:
-                for card in self.player.cards:
-                    if card.name == "No, gracias":
-                        cards_info.append(UsabilityInfoCard(
-                            cardID=card.id,
-                            name=card.name,
-                            description=card.description,
-                            usable=True
-                        ))
+            return self.get_trade_event_defense_cards()
         else:
-            # Cards that can be used to defend against the card (actions card) played by the attacker
-            for card in self.player.cards:
-                if card.name in self.get_cards_that_defend(self.card.name):
-                    cards_info.append(UsabilityInfoCard(
-                        cardID=card.id,
-                        name=card.name,
-                        description=card.description,
-                        usable=True
-                    ))
+            return self.get_action_defense_cards()
 
+    def get_trade_event_defense_cards(self):
+        cards_info = []
+        event = self.get_trade_event()
+        if event is not None and event.player2 == self.player:
+            cards_info = self.get_cards_by_name("No, gracias")
         return sorted(cards_info, key=lambda card: card.cardID)
 
-    def get_cards_that_defend(self, card_name: str) -> str:
-        """
-        Returns the type of the card
-        """
+    def get_trade_event(self):
+        incomplete_trades = select(
+            e for e in Event
+            if e.type == "trade" and e.is_completed is False)
+
+        trades_involving_player = incomplete_trades.filter(
+            lambda e: e.player1 == self.player
+            or e.player2 == self.player
+        )
+        return trades_involving_player.first()
+
+    def get_action_defense_cards(self):
+        # FIXME: cards can only be defended by a single card only
+        # this will probably break if we add more cards to the defensible_by attribute
+        defense_card = self.get_defense_cards(self.card.name)
+        return sorted(self.get_cards_by_name(defense_card),
+                      key=lambda card: card.cardID)
+
+    def get_cards_by_name(self, card_name):
+        return [
+            UsabilityInfoCard(
+                cardID=card.id,
+                name=card.name,
+                description=card.description,
+                usable=True
+            )
+            for card in self.player.cards if card.name == card_name
+        ]
+
+    def get_defense_cards(self, card_name: str) -> str:
         with open(settings.DECK_CONFIG_PATH) as config_file:
             config = json.load(config_file)
-
         return config["cards"][card_name]["defensible_by"]
 
     def handle_errors(self) -> None:
-        """
-        Checks for errors and raises HTTPException if needed
-        """
-        exceptions.validate_player_has_game( self.player, status.HTTP_400_BAD_REQUEST)
+        exceptions.validate_player_has_game(
+            self.player, status.HTTP_400_BAD_REQUEST)
         exceptions.validate_player_alive(self.player)
         if self.card is not None:
             exceptions.validate_correct_type(
