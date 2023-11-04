77609dc078b56d4271063f1086a8e6dd389eefc1
Lautaro Bachmann <laurigbach@gmail.com>
2023-11-04 19:35:05 -0300

    Refactor CardUsabilityInformer
    Replace for loop with generator expression
    
    Extract functions
    
    Improve extracted functions
    
    Refactor more code
    
    Delete unused method
    
    Remove unused imports
    
    Split function call into multiple lines
    
    Fix playability not working properly

--- src/lacosa/game/utils/card/playability_informer.py
+++ src/lacosa/game/utils/card/playability_informer.py
@@ -1,4 +1,3 @@
-import json
 from lacosa import utils
 import lacosa.game.utils.exceptions as exceptions
 from lacosa.interfaces import ResponseInterface
@@ -7,10 +6,13 @@ from lacosa.player.schemas import (
     UsabilityActionInfoCard,
 )
 from fastapi import status
-from settings import settings
 
 
 class CardUsabilityInformer(ResponseInterface):
+    UNPLAYABLE_CARDS = {"La cosa", "Infeccion"}
+    UNPLAYABLE_TYPES = {"defense"}
+    UNDISCARDABLE_CARDS = {"La cosa"}
+
     def __init__(self, player_id: int):
         self.player = utils.find_player(player_id, status.HTTP_404_NOT_FOUND)
         self.handle_errors()
@@ -23,7 +25,51 @@ class CardUsabilityInformer(ResponseInterface):
         UsabilityInfoResponse: The cards information
         """
 
-        return UsabilityActionResponse(cards=self.get_cards_info())
+        card_info = self.get_cards_info()
+        return UsabilityActionResponse(cards=card_info)
+
+    def is_card_playable(self, card):
+        return (
+            card.name not in self.UNPLAYABLE_CARDS
+            and card.type not in self.UNPLAYABLE_TYPES
+            )
+
+    def is_last_infection_card(self, card):
+        infection_cards_in_hand = self.get_infection_cards_num()
+        return (
+            self.player.role == "infected"
+            and card.name == "Infeccion"
+            and infection_cards_in_hand == 1
+        )
+
+    def is_card_discardable(self, card):
+        is_card_discardable = card.name not in self.UNDISCARDABLE_CARDS
+
+        # applies only to infected
+        is_last_infection_card = self.is_last_infection_card(card)
+
+        return (
+            is_card_discardable
+            and not is_last_infection_card
+        )
+
+    def get_usability_schema(self, card):
+        playable = self.is_card_playable(card)
+        discardable = self.is_card_discardable(card)
+
+        return UsabilityActionInfoCard(
+            cardID=card.id,
+            name=card.name,
+            description=card.description,
+            playable=playable,
+            discardable=discardable
+        )
+
+    def get_infection_cards_num(self):
+        return sum(
+            1 for card in self.player.cards
+            if card.name == "Infeccion"
+        )
 
     def get_cards_info(self) -> list:
         """
@@ -33,38 +79,10 @@ class CardUsabilityInformer(ResponseInterface):
         list: The cards information
         """
 
-        cards_info = []
-        amount_Infeccion_cards_in_hand = 0
-        for cardi in self.player.cards:
-            if cardi.name == "Infeccion":
-                amount_Infeccion_cards_in_hand += 1
-
-        for card in self.player.cards:
-            playable = True
-            discardable = True
-            if card.name == "Infeccion" or card.name == "La cosa" or self.get_card_type(card.name) == "defense":
-                playable = False
-
-            if card.name == "La cosa" or (card.name == "Infeccion" and amount_Infeccion_cards_in_hand == 1 and self.player.role == "infected"):
-                discardable = False
-
-            cards_info.append(UsabilityActionInfoCard(
-                cardID=card.id,
-                name=card.name,
-                description=card.description,
-                playable=playable,
-                discardable=discardable
-            ))
+        cards_info = [
+            self.get_usability_schema(card) for card in self.player.cards
+        ]
         return cards_info
-
-    def get_card_type(self, card_name: str) -> str:
-        """
-        Returns the type of the card
-        """
-        with open(settings.DECK_CONFIG_PATH) as config_file:
-            config = json.load(config_file)
-
-        return config["cards"][card_name]["type"]
 
     def handle_errors(self) -> None:
         """
@@ -72,5 +90,7 @@ class CardUsabilityInformer(ResponseInterface):
         """
 
         exceptions.validate_player_has_game(
-            self.player, status.HTTP_400_BAD_REQUEST)
+            self.player,
+            status.HTTP_400_BAD_REQUEST
+        )
         exceptions.validate_player_alive(self.player)
