a34cf48d76ca23de4dc024ff417e99218d722abb
Lautaro Bachmann <laurigbach@gmail.com>
2023-11-07 17:45:16 -0300

    Test shownCards appear on player status

--- src/lacosa/tests/test_get_player_data.py
+++ src/lacosa/tests/test_get_player_data.py
@@ -1,7 +1,12 @@
 from main import app
 from fastapi.testclient import TestClient
-from pony.orm import db_session
-from .game_fixtures import db_game_creation_with_cards_player_data, db_game_creation_without_cards
+from pony.orm import db_session, commit
+from .game_fixtures import (
+    db_game_creation_with_cards_player_data,
+    db_game_creation_without_cards,
+    discard_card_game_creation
+)
+from models import Player
 
 client = TestClient(app)
 
@@ -65,3 +70,29 @@ def test_get_card_not_found(db_game_creation_without_cards):
             "is_alive": True,
             "shownCards": [],
         }
+
+
+def test_show_cards(discard_card_game_creation):
+    data = discard_card_game_creation
+    player_id = data["players"][0]["id"]
+    player2_id = data["players"][1]["id"]
+    with db_session:
+        player1 = Player.get(id=player_id)
+        player2 = Player.get(id=player2_id)
+        card = player2.cards.filter().sort_by(lambda x: x.id).first()
+        player1.shown_cards.add(card)
+        commit()
+        response = client.get(f"/player/{player_id}")
+        response_json = response.json()
+        assert response.status_code == 200
+        assert response_json["hand"] == [
+            {'cardID': 1, 'name': 'card1', 'description': 'Generic card description'},
+            {'cardID': 2, 'name': 'card2', 'description': 'Generic card description'},
+            {'cardID': 3, 'name': 'card3', 'description': 'Generic card description'},
+            {'cardID': 4, 'name': 'card4', 'description': 'Generic card description'}
+        ]
+        assert response_json["role"] == "human"
+        assert response_json["is_alive"] is True
+        assert response_json["shownCards"] == [
+            {'cardID': 5, 'name': 'card5', 'description': 'Generic card description'}
+        ]
