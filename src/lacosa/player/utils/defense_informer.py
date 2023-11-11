c7469e0494d35b8c4045b2d54be1cdf257d60752
ValentinoMensio <valentino.mensio@mi.unc.edu.ar>
2023-11-10 21:06:30 -0300

    fix defense_informer

--- src/lacosa/player/utils/defense_informer.py
+++ src/lacosa/player/utils/defense_informer.py
@@ -15,7 +15,9 @@ from settings import settings
 class CardDefenseInformer(ResponseInterface):
     def __init__(self, player_id: int, card_id: int):
         self.player = utils.find_player(player_id, status.HTTP_404_NOT_FOUND)
-        self.card = utils.find_card(card_id, status.HTTP_404_NOT_FOUND)
+        self.card = None
+        if self.card != -1:
+            self.card = utils.find_card(card_id, status.HTTP_404_NOT_FOUND)
         self.handle_errors()
 
     def get_response(self) -> UsabilityResponse:
@@ -37,12 +39,11 @@ class CardDefenseInformer(ResponseInterface):
 
     def get_trade_event(self):
         incomplete_trades = select(
-            e for e in Event
-            if e.type == "trade" and e.is_completed is False)
+            e for e in Event if e.type == "trade" and e.is_completed is False
+        )
 
         trades_involving_player = incomplete_trades.filter(
-            lambda e: e.player1 == self.player
-            or e.player2 == self.player
+            lambda e: e.player1 == self.player or e.player2 == self.player
         )
         return trades_involving_player.first()
 
@@ -50,8 +51,9 @@ class CardDefenseInformer(ResponseInterface):
         # FIXME: cards can only be defended by a single card only
         # this will probably break if we add more cards to the defensible_by attribute
         defense_card = self.get_defense_cards(self.card.name)
-        return sorted(self.get_cards_by_name(defense_card),
-                      key=lambda card: card.cardID)
+        return sorted(
+            self.get_cards_by_name(defense_card), key=lambda card: card.cardID
+        )
 
     def get_cards_by_name(self, card_name):
         return [
@@ -59,9 +61,10 @@ class CardDefenseInformer(ResponseInterface):
                 cardID=card.id,
                 name=card.name,
                 description=card.description,
-                usable=True
+                usable=True,
             )
-            for card in self.player.cards if card.name == card_name
+            for card in self.player.cards
+            if card.name == card_name
         ]
 
     def get_defense_cards(self, card_name: str) -> str:
@@ -70,9 +73,7 @@ class CardDefenseInformer(ResponseInterface):
         return config["cards"][card_name]["defensible_by"]
 
     def handle_errors(self) -> None:
-        exceptions.validate_player_has_game(
-            self.player, status.HTTP_400_BAD_REQUEST)
+        exceptions.validate_player_has_game(self.player, status.HTTP_400_BAD_REQUEST)
         exceptions.validate_player_alive(self.player)
         if self.card is not None:
-            exceptions.validate_correct_type(
-                self.card, "action")
+            exceptions.validate_correct_type(self.card, "action")
